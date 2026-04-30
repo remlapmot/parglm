3902e700fcdeb2816c354cc4085efa46eb9515f5
Tom Palmer <remlapmot@hotmail.com>
2026-04-30 08:00:15 +0100

    Update precompile.R

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -1,2 +1,7 @@
+# Precompile vignette
+# Must move image files from top level to vignettes/ after knit
+
 if (!requireNamespace("parglm")) stop("Remember to install parglm itself!")
 knitr::knit("vignettes/parglm.Rmd.orig", output = "vignettes/parglm.Rmd")
+pngs <- c("figure/show_run_times-1.png")
+file.rename(pngs, file.path("vignettes", pngs))

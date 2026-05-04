41547f3ecd08ee48a9b0bb4c7cfcc115baeb91ed
Tom Palmer <remlapmot@hotmail.com>
2026-05-04 15:09:45 +0100

    Move all pngs from figure/

--- vignettes/precompile.R
+++ vignettes/precompile.R
@@ -3,5 +3,9 @@
 
 if (!requireNamespace("parglm")) stop("Remember to install parglm itself!")
 knitr::knit("vignettes/parglm.Rmd.orig", output = "vignettes/parglm.Rmd")
-pngs <- c("figure/show_run_times-1.png")
-file.rename(pngs, file.path("vignettes", pngs))
+pngs <- list.files("figure", pattern = "\\.png$", full.names = TRUE)
+if (length(pngs) > 0) {
+  dir.create("vignettes/figure", showWarnings = FALSE, recursive = TRUE)
+  file.rename(pngs, file.path("vignettes", pngs))
+  unlink("figure", recursive = TRUE)
+}
